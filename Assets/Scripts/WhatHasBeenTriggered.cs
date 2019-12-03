a8b2deaa395de97473ef71ecfbc8ec725c70cc78
Jamie2Otters <32065389+Jamie2Otters@users.noreply.github.com>
2019-12-03 11:13:48 -0500

    progress!!

---
+++ Assets/Scripts/WhatHasBeenTriggered.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatHasBeenTriggered : MonoBehaviour
+{
+    public bool keyCheck = false;
+    public GameObject key;
+    private void Update()
+    {
+        if(GameObject.Find("key") != null)
+        {
+
+        }
+    }
+}
