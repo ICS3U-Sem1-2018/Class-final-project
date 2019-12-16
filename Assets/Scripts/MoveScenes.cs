28a1bb489f1f322c9797beb72a2ff3c2303bb870
Jamie2Otters <32065389+Jamie2Otters@users.noreply.github.com>
2019-12-16 12:08:27 -0500

    work in progress

--- Assets/Scripts/MoveScenes.cs
+++ Assets/Scripts/MoveScenes.cs
@@ -1,17 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MoveScenes : MonoBehaviour
 {
+    private InventorySystem inventory;
+    public Item item1;
+    public Item item2;
     [SerializeField] private string newLevel;
+    [SerializeField] private string Item1Level;
+    [SerializeField] private string Item2Level;
 
+
+    private void Update()
+    {
+
+
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(newLevel);
+
+            inventory = FindObjectOfType<InventorySystem>();
+
+            if (item1 == true)
+            {
+                SceneManager.LoadScene(Item1Level);
+            }
+
+            else if (item2 == true)
+            {
+                SceneManager.LoadScene(Item2Level);
+            }
+            return;
         }
+        
     }
+
 }
