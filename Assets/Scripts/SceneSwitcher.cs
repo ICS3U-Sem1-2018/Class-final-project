7c23f102114e6b364673b038aebc968b7ea0d990
Jamie2Otters <32065389+Jamie2Otters@users.noreply.github.com>
2020-01-08 14:36:01 -0500

    Interactive items are all thats left to add

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    [SerializeField] private string next;
+
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene(newLevel);
+        return;
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene(next);
+        return;
+    }
+}
+
