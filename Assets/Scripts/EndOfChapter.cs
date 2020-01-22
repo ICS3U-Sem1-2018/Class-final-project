0719b923287872c6110e073aaec17d4374055808
Jamie2Otters <32065389+Jamie2Otters@users.noreply.github.com>
2020-01-22 07:53:18 -0500

    still going

---
+++ Assets/Scripts/EndOfChapter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfChapter : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    public bool itemCheck = false;
+
+    private void Update()
+    {
+        if (itemCheck == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+
+                SceneManager.LoadScene(newLevel);
+
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player found");
+            itemCheck = true;
+        }
+        return;
+    }
+   
+
+    
+}
